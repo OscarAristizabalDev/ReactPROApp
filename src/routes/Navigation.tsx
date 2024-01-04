49475187751b0d83360e063f2ad5f36e1bc3b2f8
OscarAristizabalDev <oscar.aristi.jaimes@gmail.com>
2024-01-04 15:33:46 -0500

    Component ShoppingPage without refactoring using compound component pattern

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate } from "react-router-dom";
 import { Routes, Route, NavLink } from "react-router-dom";
 
 import logo from "../assets/react.svg"
+import { ShoppingPage } from "../02-component-patterns/pages/ShoppingPage";
 
 export const Navigation = () => {
     return (
@@ -11,7 +12,7 @@ export const Navigation = () => {
                     <img src={logo} alt="React logo" />
                     <ul>
                         <li>
-                            <NavLink to="/home" className={({ isActive }) => isActive ? "nav-active" : ""}>Home</NavLink>
+                            <NavLink to="/" className={({ isActive }) => isActive ? "nav-active" : ""}>Shopping</NavLink>
                         </li>
                         <li>
                             <NavLink to="/about" className={({ isActive }) => isActive ? "nav-active" : ""}>About</NavLink>
@@ -24,7 +25,7 @@ export const Navigation = () => {
                 <Routes>
                     <Route path="about" element={<h1>About</h1>} />
                     <Route path="users" element={<h1>users</h1>} />
-                    <Route path="home" element={<h1>home</h1>} />
+                    <Route path="/" element={<ShoppingPage />} />
 
                     <Route path="/*" element={<Navigate to="/home" replace />} />
                 </Routes>
